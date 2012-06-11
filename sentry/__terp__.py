f0f1e5f46b0a27beae04158bcbcd0bc20d9d38ee
Eduard Carreras <ecarreras@gisce.net>
2012-06-11 14:40:20 +0000

    Added license and copyright

--- sentry/__terp__.py
+++ sentry/__terp__.py
@@ -5,6 +5,7 @@
     "depends": ["base"],
     "author": "GISCE",
     "category": "Tools",
+    "license": "GPL-3'"
     "description": """
     This module provide :
         * Log events to sentry server: http://www.getsentry.com
--- sentry/sentry.py
+++ sentry/sentry.py
@@ -1,4 +1,7 @@
 # -*- coding: utf-8 -*-
+# This file is part of openerp-sentry. The COPYRIGHT file at the top level of
+# this repository contains the full copyright notices and license terms.
+
 from osv import osv
 from tools import config
 from tools.translate import _
