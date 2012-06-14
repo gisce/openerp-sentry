0e9109a37ee0f5d789bcd364616e31153056ed14
Eduard Carreras <ecarreras@gisce.net>
2012-06-14 11:14:21 +0000

    FIX __terp__ format

--- sentry/__terp__.py
+++ sentry/__terp__.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
 {
     "name": "Sentry",
-    "version": "0.1.0",
+    "version": "0.1.1",
     "depends": ["base"],
     "author": "GISCE",
     "category": "Tools",
-    "license": "GPL-3'"
+    "license": "GPL-3",
     "description": """
     This module provide :
         * Log events to sentry server: http://www.getsentry.com
